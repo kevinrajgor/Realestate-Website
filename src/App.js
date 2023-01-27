53605b5efba05651925ac0ddd2e0a81508fca08c
kevinrajgor <kevinrajg95@gmail.com>
2023-01-27 16:48:06 +0530

    Added pages and Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Profile from "./Pages/Profile";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import Offers from "./Pages/Offers";
+import ForgotPassword from "./Pages/ForgotPassword";
 function App() {
   return (
     <>
-      <h1>Hello I'm Kevin!</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/offers" element={<Offers />} />
+        </Routes>
+      </Router>
     </>
   );
 }
