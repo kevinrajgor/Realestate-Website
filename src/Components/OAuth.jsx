431b9a20279395318d59c3faddb94a5aef54b094
kevinrajgor <kevinrajg95@gmail.com>
2023-01-30 17:13:26 +0530

    Added Google Pop-Up login Functionality

--- src/Components/OAuth.jsx
+++ src/Components/OAuth.jsx
@@ -1,9 +1,42 @@
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
+import { useNavigate } from "react-router";
+import { toast } from "react-toastify";
+import { db } from "../Firebase";
 
 export default function OAuth() {
+  const navigate = useNavigate();
+  async function onGoogleClick() {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      //checking if user already exits
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+      toast.success("Successfully logged in!");
+      navigate("/");
+    } catch (error) {
+      toast.error("Could not authorize Google!");
+    }
+  }
   return (
-    <button className="flex items-center justify-center w-full bg-red-600 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-700 active:bg-red-800 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded-lg">
+    <button
+      type="button" //made it button such that no error comes, as Oauth is inside the form.
+      onClick={onGoogleClick}
+      className="flex items-center justify-center w-full bg-red-600 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-700 active:bg-red-800 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded-lg"
+    >
       <FcGoogle className="text-2xl bg-white rounded-full mr-2" />
       Continue with Google
     </button>
