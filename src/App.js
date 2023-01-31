b615366ae4cab8fe099046a000377ef1bfa510f2
kevinrajgor <kevinrajg95@gmail.com>
2023-01-31 15:42:24 +0530

    Added Private route component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./Pages/Home";
 import Profile from "./Pages/Profile";
 import SignIn from "./Pages/SignIn";
 import SignUp from "./Pages/SignUp";
+import PrivateRoute from "./Components/PrivateRoute";
 import Offers from "./Pages/Offers";
 import ForgotPassword from "./Pages/ForgotPassword";
 import Header from "./Components/Header";
@@ -16,7 +17,9 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
