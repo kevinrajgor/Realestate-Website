97779629ddd9e5b10d4c10655ee47bbaa69d6e67
kevinrajgor <kevinrajg95@gmail.com>
2023-02-01 15:23:42 +0530

    Header - completed it and made it dynamic

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router"; //Provides the current object, helps know the location/pathname of the user.
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 export default function Header() {
+  const [pageState, setPageState] = useState("Sign-In");
   const location = useLocation();
   const navigate = useNavigate(); //to help navigate from one path to other without refreshing the page
   //   console.log(location.pathname); Gives the path of the user.
-  function pathMathRoute(route) {
+  const auth = getAuth();
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setPageState("Profile");
+      } else {
+        setPageState("Sign-In");
+      }
+    });
+  });
+
+  function pathMatchRoute(route) {
     if (route === location.pathname) {
       return true;
     }
@@ -25,24 +38,27 @@ export default function Header() {
           <ul className="flex space-x-10">
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent 
-              ${pathMathRoute("/") && "text-black border-b-red-500"}`}
+              ${pathMatchRoute("/") && "text-black border-b-red-500"}`}
               onClick={() => navigate("/")}
             >
               Home
             </li>
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent 
-              ${pathMathRoute("/offers") && "text-black border-b-red-500"}`}
+              ${pathMatchRoute("/offers") && "text-black border-b-red-500"}`}
               onClick={() => navigate("/offers")}
             >
               Offers
             </li>
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent 
-              ${pathMathRoute("/sign-in") && "text-black border-b-red-500"}`}
-              onClick={() => navigate("/sign-in")}
+              ${
+                (pathMatchRoute("/sign-in") || pathMatchRoute("/profile")) &&
+                "text-black border-b-red-500"
+              }`}
+              onClick={() => navigate("/profile")} //onclick is rerouting the page to profile even when it is on sign-in but as condition is false it does not
             >
-              Sign In
+              {pageState}
             </li>
           </ul>
         </div>
