7f774e3dd55846b51a28300391cd31d290473ec6
kevinrajgor <kevinrajg95@gmail.com>
2023-02-06 15:22:09 +0530

    CreateListing- Minor code changes

--- src/Pages/CreateListing.jsx
+++ src/Pages/CreateListing.jsx
@@ -158,6 +158,7 @@ export default function CreateListing() {
       imgUrls,
       geoLocation,
       timestamp: serverTimestamp(),
+      userRef: auth.currentUser.uid,
     };
     delete formDataCopy.images;
     !formDataCopy.offer && delete formDataCopy.discountedPrice;
